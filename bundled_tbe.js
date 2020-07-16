be9f837b2270f32edf2aa67009b07d4507d7a5b8
axk5209 <arhamkothari@utexas.edu>
2020-07-15 23:38:31 -0500

    Added Link toto Deep Link for troubleshooting

--- bundled_tbe.js
+++ bundled_tbe.js
@@ -7295,7 +7295,7 @@ module.exports = E;
 module.exports.TinyEmitter = E;
 
 },{}],13:[function(require,module,exports){
-"use strict";
+'use strict';
 
 /*
 Copyright (c) 2020 Trashbots - SDG
@@ -7322,18 +7322,21 @@ SOFTWARE.
 module.exports = function () {
 
 	//const oldAlert = window.alert
-	window.alert = function () {};
-	window.onload = function () {
-		var isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
-		if (isMobile) {
-			window.location.href = "tblocks://";
-			setTimeout(function () {
-				window.location.href = "http://tblocks.app.link";
-			}, 1000);
-		} else {
-			window.location.href = "https://trashbots.github.io/tblocks";
-		}
-	};
+	/*
+ window.alert = function () {}
+ window.onload = function() {
+ 	const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+ 	if (isMobile)
+ 	{
+ 		window.location.href = "tblocks://"
+ 		setTimeout(() => {window.location.href = "http://tblocks.app.link"}, 1000)		
+ 	}
+ 	else
+ 	{
+ 		window.location.href = "https://trashbots.github.io/tblocks"
+ 	}
+ }
+ */
 	//window.alert = oldAlert
 
 
@@ -7428,7 +7431,7 @@ module.exports = function () {
 		var cookieSheet = document.getElementById('cookieSheet');
 		var cookiesAccepted = app.storage.getItem('cookiesAccepted');
 		if (!isApp && (cookiesAccepted === null || cookiesAccepted === false)) {
-			cookieSheet.innerHTML = "\n        <div id='cookiesGlass'></dev>\n        <div id='cookiesForm'>\n            <div id='cookiesNote'>\n              <input id='cookiesButton' type=\"button\" value=\"  Accept Cookies  \" style=\"float:right\">\n              <p>\n                  We use cookies and similar technologies for document\n                  stroage functionality and to measure performance of application features.\n                  You consent to our cookies if you continue to use our website.\n              </p>\n            </div>\n        </div>\n        ";
+			cookieSheet.innerHTML = '\n        <div id=\'cookiesGlass\'></dev>\n        <div id=\'cookiesForm\'>\n            <div id=\'cookiesNote\'>\n\t\t\t  <input id=\'cookiesButton\' type="button" value="  Accept Cookies  " style="float:right">\n\t\t\t  <a href="https://tblocks.app.link">Deep Link</a>\n              <p>\n                  We use cookies and similar technologies for document\n                  stroage functionality and to measure performance of application features.\n                  You consent to our cookies if you continue to use our website.\n              </p>\n            </div>\n        </div>\n        ';
 			var cookiesButton = document.getElementById('cookiesButton');
 			cookiesButton.onclick = app.hideCookieSheet;
 		}
